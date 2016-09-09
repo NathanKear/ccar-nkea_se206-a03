b5b58af694ec2fcf9a882c9067df92cada573300
Nathan <n.j.kear@gmail.com>
2016-09-10 10:06:26 +1200

    Added buttons and welcome text to main menu screen

--- src/VoxspellPrototype/VoxspellPrototype.java
+++ src/VoxspellPrototype/VoxspellPrototype.java
@@ -1,13 +1,25 @@
 package VoxspellPrototype;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class VoxspellPrototype extends Application {
 
 	private Stage _window;
 	
-	private final String _windowTitle = "VOXSPELL(Hey Nathan! I hope this works!)";
+	private int _width, _height;
+	
+	private final String WINDOW_TITLE = "VOXSPELL(Hey Nathan! I hope this works!)";
+	private final int BUTTON_SEPERATION = 6; 
+	
+	private String _displayText = "Hello World!";
 	
 	public VoxspellPrototype() {
 	}
@@ -15,13 +27,52 @@ public class VoxspellPrototype extends Application {
 	@Override
 	public void start(Stage window) throws Exception {
 		this._window = window;
+		_height = 200;
+		_width = 500;
 		
-		_window.setTitle(_windowTitle);
+		HBox root = new HBox(0);
+		Pane menuPane = buildMenuBar();
+		Text welcomeText = new Text(_displayText);
+		
+		welcomeText.setTextAlignment(TextAlignment.CENTER);
+		welcomeText.setWrappingWidth(_width 
+				- menuPane.getPrefWidth() 
+				- menuPane.getPadding().getLeft() 
+				- menuPane.getPadding().getRight());
+		root.getChildren().addAll(menuPane, welcomeText);
+		
+		_window.setScene(new Scene(root, _width, _height));
+		
+		_window.setTitle(WINDOW_TITLE);
 		_window.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+	
+	private Pane buildMenuBar() {
+		Button btnNew, btnReview, btnStats, btnClear, btnQuit;
+		
+		VBox menuButtons = new VBox(BUTTON_SEPERATION);
+		
+		// Create buttons
+		btnNew = new Button("New Quiz");
+		btnReview = new Button("Review Mistakes");
+		btnStats = new Button("View Stats");
+		btnClear = new Button("Clear Stats");
+		btnQuit = new Button("Quit");
+		
+		// Set width and height of buttons
+		btnNew.setMinWidth(menuButtons.getPrefWidth()); btnNew.setPrefHeight(Integer.MAX_VALUE);
+		btnReview.setMinWidth(menuButtons.getPrefWidth()); btnReview.setPrefHeight(Integer.MAX_VALUE);
+		btnStats.setMinWidth(menuButtons.getPrefWidth()); btnStats.setPrefHeight(Integer.MAX_VALUE);
+		btnClear.setMinWidth(menuButtons.getPrefWidth()); btnClear.setPrefHeight(Integer.MAX_VALUE);
+		btnQuit.setMinWidth(menuButtons.getPrefWidth()); btnQuit.setPrefHeight(Integer.MAX_VALUE);
+		
+		// Add buttons to pane
+		menuButtons.getChildren().addAll(btnNew, btnReview, btnStats, btnClear, btnQuit);
+		
+		return menuButtons;
+	}
 }
